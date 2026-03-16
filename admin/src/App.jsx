4257b135a3a070583012948b69f89faf84818aae
siva priya <nspriya005@gmail.com>
2026-03-16 22:32:07 +0530

    20th commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,48 +1,5 @@
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Sidebar from './components/Sidebar';
-// import AdminLogin from './components/AdminLogin';
-// import Products from './pages/Products';
-// import Users from './pages/Users';
-// import Orders from './pages/Orders';
-
-// const App = () => {
-//   return (
-   
-
-//   <Routes>
-
-//     <Route path="/admin" element={<AdminLogin />} />
-
-   
-//     <Route
-//       path="/*"
-//       element={
-//         <div className="flex">
-//           <Sidebar />
-//           <div className="flex-1">
-//             <Navbar />
-//             <Routes>
-//               <Route path="products" element={<Products />} />
-//               <Route path="users" element={<Users />} />
-//               <Route path="orders" element={<Orders />} />
-//             </Routes>
-//           </div>
-//         </div>
-//       }
-//     />
-
-//   </Routes>
-
-
-//   );
-// }
-
-// export default App;
-
 import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import AdminLogin from "./components/AdminLogin";
@@ -50,13 +7,13 @@ import Products from "./pages/Products";
 import Users from "./pages/Users";
 import Orders from "./pages/Orders";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1">
         <Navbar />
-        <Outlet />
+        {children}
       </div>
     </div>
   );
@@ -66,15 +23,36 @@ const App = () => {
   return (
     <Routes>
 
-      {/* Admin Login */}
-      <Route path="/admin" element={<AdminLogin />} />
+      {/* First Page → Login */}
+      <Route path="/" element={<AdminLogin />} />
 
-      {/* Admin Panel */}
-      <Route path="/admin" element={<Layout />}>
-        <Route path="products" element={<Products />} />
-        <Route path="users" element={<Users />} />
-        <Route path="orders" element={<Orders />} />
-      </Route>
+      {/* Admin Pages */}
+      <Route
+        path="/products"
+        element={
+          <Layout>
+            <Products />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/users"
+        element={
+          <Layout>
+            <Users />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/orders"
+        element={
+          <Layout>
+            <Orders />
+          </Layout>
+        }
+      />
 
     </Routes>
   );
