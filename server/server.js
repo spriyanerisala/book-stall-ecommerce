6bbcde28f820258145489f161f312bb3afc95583
siva priya <nspriya005@gmail.com>
2026-03-16 22:02:02 +0530

    15th commit

--- server/server.js
+++ server/server.js
@@ -1,38 +1,56 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-import authRouter from './routes/authRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import { stripeWebhook } from './controllers/paymentController.js';
-import orderRouter from './routes/orderRoutes.js';
-import commentRouter from './routes/commentRoutes.js';
-import { redis } from './config/redis.js';
-import { connectDB } from './config/db.js';
-import { preloadProductsToRedis,preloadUsersToRedis ,preloadCommentsToRedis,preloadOrdersToRedis } from './preloadData/preloadData.js';
-import cartRouter from './routes/cartRoutes.js';
-import paymentRouter from './routes/paymentRoute.js';
-import adminRouter from './routes/adminRoutes.js';
+import authRouter from "./routes/authRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import { stripeWebhook } from "./controllers/paymentController.js";
+import orderRouter from "./routes/orderRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+import paymentRouter from "./routes/paymentRoute.js";
+import adminRouter from "./routes/adminRoutes.js";
+
+import { redis } from "./config/redis.js";
+import { connectDB } from "./config/db.js";
+import {
+  preloadProductsToRedis,
+  preloadUsersToRedis,
+  preloadCommentsToRedis,
+  preloadOrdersToRedis
+} from "./preloadData/preloadData.js";
+
 const app = express();
+
+
+app.use(
+  cors({
+    origin: [
+      process.env.CLIENT_URL,
+      process.env.ADMIN_URL
+    ],
+    credentials: true
+  })
+);
 
 
 
 app.post(
-  '/api/payment/webhook',
-  express.raw({ type: 'application/json' }),
+  "/api/payment/webhook",
+  express.raw({ type: "application/json" }),
   stripeWebhook
 );
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin:[process.env.CLIENT_URL,process.env.ADMIN_URL],
 
-  credentials:true
-}));
+
 
 await connectDB();
+
 
 
 (async () => {
@@ -45,18 +63,21 @@ await connectDB();
 })();
 
 
-app.use('/api/auth', authRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders',orderRouter);
-app.use('/api/comments',commentRouter);
-app.use('/api/cart',cartRouter);
-app.use('/api/payment',paymentRouter);
-app.use('/api/admin',adminRouter);
 
-app.get('/', (req, res) => {
+app.use("/api/auth", authRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/comments", commentRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/payment", paymentRouter);
+app.use("/api/admin", adminRouter);
+
+
+
+app.get("/", (req, res) => {
   res.send("Welcome to Book Stall E-commerce API");
 });
- 
+
 
 
 preloadUsersToRedis();
@@ -65,7 +86,9 @@ preloadCommentsToRedis();
 preloadOrdersToRedis();
 
 
+
 const port = process.env.PORT || 5000;
-app.listen(port,"0.0.0.0" ,() => {
-  console.log(`Server is running on : http://0.0.0.0:${port}`);
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
